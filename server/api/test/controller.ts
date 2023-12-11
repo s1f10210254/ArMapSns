04d0b0d0ab3621a4031baa95d84edc06689cbb53
s1f10210254 <s1f10210248@iniad.org>
2023-12-11 19:50:10 +0900

    写真投稿機能を備えたapiは完成

--- server/api/test/controller.ts
+++ server/api/test/controller.ts
@@ -1,5 +1,37 @@
+import { photoRepository } from '$/repository/photoRepository';
+import { postPost, postPostAndPhoto } from '$/repository/postRepository';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: () => ({ status: 200, body: 'Hello' }),
+  // get: () => ({ status: 200, body: 'Hello' }),
+  post: async ({ body }) => {
+    try {
+      const file = body.file;
+      if (file !== undefined) {
+        const fileBuffer = await file.toBuffer();
+        const url = await photoRepository.uploadPOSTPhoto(body.username, fileBuffer, file.mimetype);
+        const result = await postPostAndPhoto(
+          body.username,
+          body.content,
+          body.latitude,
+          body.longitude,
+          body.userID,
+          url
+        );
+        return { status: 200, body: result };
+      } else {
+        const result = await postPost(
+          body.username,
+          body.content,
+          body.latitude,
+          body.longitude,
+          body.userID
+        );
+        return { status: 200, body: result };
+      }
+    } catch (error) {
+      console.error(error);
+      return { status: 500, body: { message: 'アップロード失敗' } };
+    }
+  },
 }));
--- server/api/test/index.ts
+++ server/api/test/index.ts
@@ -1,7 +1,18 @@
 import type { DefineMethods } from 'aspida';
 
 export type Methods = DefineMethods<{
-  get: {
-    resBody: string;
+  // get: {
+  //   resBody: string;
+  // };
+  post: {
+    reqFormat: FormData;
+    reqBody: {
+      username: string;
+      content: string;
+      latitude: number;
+      longitude: number;
+      userID: string;
+      file?: Blob;
+    };
   };
 }>;
